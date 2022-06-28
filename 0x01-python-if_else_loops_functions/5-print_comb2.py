72155ad43b4d2777a19800b673ad3120d58bb1d4
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-06-28 18:16:16 +0000

    Print from 0 to 98

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,5 @@
 for i in range(0, 100):
     if i == 99:
         print("{}".format(i))
-    if i >= 0 and i <= 9:
-        print(f"0{i}")
     else:
-        print("{}, ".format(i))
+        print("{:02}, ".format(i), end='')
