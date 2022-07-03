33a25589e3f94891e56607b65faf3d49295aa333
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-07-03 15:40:14 +0000

    Remove cC

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def no_c(my_string):
-    new_string = ([i for i in my_string if i != 'C' and i != 'c'])
+    new_string = [d for d in my_string if d != 'C' and d != 'c']
     return ("".join(new_string))
