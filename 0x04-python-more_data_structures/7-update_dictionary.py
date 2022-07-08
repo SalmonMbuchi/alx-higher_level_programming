f16d75d5b0737cdbb2b98fe5df6a9914899d4fcd
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-07-08 20:05:09 +0000

    Update a dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for x in list(a_dictionary):
-        if key in a_dictionary and x is key:
-            a_dictionary[x] = value
-        else:
-            a_dictionary[key] = value
+    a_dictionary[key] = value
     return a_dictionary
