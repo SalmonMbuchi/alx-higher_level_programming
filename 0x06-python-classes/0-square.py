1d028f8c1bedfabfee2e445d0d32e1946e3bbb29
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-07-20 09:09:21 +0000

    Create an empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# 0-square.py
+""" This task involves creating an empty class the defines a square"""
 class Square:
     """ create an empty class square that defines a square """
     pass
