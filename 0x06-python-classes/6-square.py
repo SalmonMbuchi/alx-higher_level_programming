d7eb449c88270a40f8db2132113af83ff7056264
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-07-20 17:34:57 +0000

    Print a square pattern with whitespace

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -55,6 +55,8 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
+        for i in range(0, self.__position[1]):
+            print()
         else:
             for i in range(0, self.__size):
                 for k in range(0, self.__position[0]):
