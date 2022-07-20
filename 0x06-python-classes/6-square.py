f37e177a7c5d7ed75de11de7455017c2a20ad0af
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-07-20 18:59:52 +0000

    Print a square pattern with whitespace

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -40,11 +40,10 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not (isinstance(value, tuple) or
-                isinstance(value[0], int) or
-                len(value) == 2 or
-                isinstance(value[1], int) or
-                value[1] >= 0 or value[1] >= 0):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TyperError("position must be a tuple of two integers")
 
         self.__position = value
