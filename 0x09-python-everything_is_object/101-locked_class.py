28b790930644b4b386b72309b5f93d1b8044f49c
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-07-27 18:24:05 +0000

    Set up a locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """define a locked class"""
 
+
 class LockedClass:
     """a class with no class or object attributes that prevents
     the dynamic creation of new instance attributes except 'first_name'"""
