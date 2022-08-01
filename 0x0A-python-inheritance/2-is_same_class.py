f95e95b756c7212391dc8c7b44e9687ef61f7644
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-08-01 16:49:22 +0000

    Corrections

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,4 +4,6 @@
 
 def is_same_class(obj, a_class):
     """return True if the object is an exact instance, else return False"""
-    return isinstance(obj, a_class)
+    if type(obj) == a_class:
+        return True
+    return False
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,6 @@
 
 def inherits_from(obj, a_class):
     """return True if object is an instance else return fALSE"""
-    return isinstance(obj, a_class)
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
