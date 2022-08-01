b9924d4ae36b3a0a9c99081ebec7a5dc80d0abae
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-08-01 13:25:12 +0000

    Raise an exception with a message

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""an empty class"""
+
+
+class BaseGeometry:
+    """empty class"""
+    def area(self):
+        """raise an exception"""
+        raise Exception("area() is not implemented")
