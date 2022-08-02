493a9bb16dbcd25ccc409f5b3322aff74b4d1eb7
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-08-02 20:03:25 +0000

    Correction to 8-class

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""return dictionary description"""
-import json
+"""Defines a Python class-to-JSON function."""
 
 
 def class_to_json(obj):
-    """returns dictionary description with data structure"""
+    """Return the dictionary represntation of a simple data structure."""
     return obj.__dict__
