dede8465d530198b890327b61fb98bbba0f4037a
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-08-09 09:23:09 +0000

    Commit changes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -135,6 +135,7 @@ class Rectangle(Base):
 
     # return the dictionary representation of a Rectangle
     def to_dictionary(self):
+        """returns the dictionary representation of a Rectangle"""
         return {'x': self.__x, 'y': self.__y, 'id': self.id,
                 'height': self.__height, 'width': self.__width}
 
