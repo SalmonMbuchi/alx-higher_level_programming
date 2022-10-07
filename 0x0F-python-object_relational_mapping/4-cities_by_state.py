9253caebcb0a6b796e3ba85f6c4fa773d9fc3ebe
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-10-07 22:38:20 +0300

    Spaces v tabs

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""lists all cities"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT cities.id, cities.name, states.name\
+        FROM cities INNER JOIN states\
+        ORDER BY cities.id")
+    for city in cur.fetchall():
+        print(city)
+    cur.close()
+    conn.close()
