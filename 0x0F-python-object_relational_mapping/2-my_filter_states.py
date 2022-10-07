ed06c011d5971c21a22284d7934bf9bbcf2a9ceb
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-10-07 21:59:37 +0300

    Multi-line statements

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
-    param = (sys.argv[4],)
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", param)
+    cur.execute(
+        "SELECT * FROM states WHERE \
+        BINARY name = '{}' ORDER BY id ASC".format(sys.argv[4]))
     states = cur.fetchall()
     for state in states:
         print(state)
