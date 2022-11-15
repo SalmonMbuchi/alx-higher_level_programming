3b0395c831daaaa84bdcffe6b30c740a6b933867
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-11-15 22:17:20 +0300

    more advanced application

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,10 +5,4 @@ const fs = require('fs');
 const request = require('request');
 const url = process.argv[2];
 const file = process.argv[3];
-request(url, (error, response, body) => {
-  if (error) console.log(error);
-  const content = JSON.parse(JSON.stringify(body.trim()));
-  fs.writeFile(file, content, (err) => {
-    if (err) console.log(err);
-  });
-});
+request(url).pipe(fs.createWriteStream(file));
--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,3 +2,17 @@
 // Compute the number of tasks completed by user id
 
 const request = require('request');
+const url = process.argv[2];
+request(url, (error, response, body) => {
+  if (error) console.log(error);
+  const tasks = JSON.parse(body);
+  const completed = {};
+  tasks.forEach((task) => {
+    if (task.completed && completed[task.userId] === undefined) {
+      completed[task.userId] = 1;
+    } else if (task.completed) {
+      completed[task.userId] += 1;
+    }
+  });
+  console.log(completed);
+});
