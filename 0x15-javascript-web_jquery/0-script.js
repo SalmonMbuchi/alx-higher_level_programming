6a4de5d58c775258f0884e8285b10b586eb1c307
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-11-23 23:35:01 +0300

    initial upload

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+const para = document.querySelector('header');
+
+para.style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/README.md
@@ -0,0 +1,3 @@
+# 0x15-javascript-web_jquery
+
+In this project, I learnt about jQuery and its functions.
